7f0eac2f6c1ff8a4f9ec4267998bc10a4ea47f58
VaheYeritsyan <vahe.yeristyan.2001@gmail.com>
2024-04-11 13:27:34 +0400

    Initial commit

---
+++ src/main/java/com/capstone/GhumDoZa/GhumDoZaApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.GhumDoZa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhumDoZaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhumDoZaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capstone/GhumDoZa/GhumDoZaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.GhumDoZa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhumDoZaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
